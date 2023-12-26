1dd496988d2a2514aec3d7be5ff7bc937c3b3602
Tom Hockett <tom.hockett@gmail.com>
2023-12-26 14:20:12 -0600

    add refresh session endpoint

--- lib/elixir_api_web/auth/error_response.ex
+++ lib/elixir_api_web/auth/error_response.ex
@@ -5,3 +5,7 @@ end
 defmodule ElixirApiWeb.Auth.ErrorResponse.Forbidden do
   defexception message: "You do not have access to this resource.", plug_status: 403
 end
+
+defmodule ElixirApiWeb.Auth.ErrorResponse.NotFound do
+  defexception message: "Resource not found.", plug_status: 404
+end
--- lib/elixir_api_web/router.ex
+++ lib/elixir_api_web/router.ex
@@ -36,6 +36,7 @@ defmodule ElixirApiWeb.Router do
     pipe_through [:api, :auth]
     get "/accounts/by_id/:id", AccountController, :show
     get "/accounts/sign_out", AccountController, :sign_out
+    get "/accounts/refresh_session", AccountController, :refresh_session
     post "/accounts/update", AccountController, :update
   end
 end
